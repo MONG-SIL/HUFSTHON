2f41e6fdb825607a8700c7a584b0729ead8a9a4d
MONG-SIL <abok2836@naver.com>
2024-11-16 15:00:05 +0900

    resolve merge

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
+  
   <StrictMode> 
-=======
-  <StrictMode>
->>>>>>> 483cedf6e224e4e5b9a2dccddeb6ae88241729a9
     <App />
   </StrictMode>,
 )
